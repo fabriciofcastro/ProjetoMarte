eb9e3b6746cd70df50b78e250f842f1010c3a022
Fabricio Castro <fabriciofeitosacastro@gmail.com>
2022-09-08 07:55:35 -0300

    implementação da página Sobre

--- src/components/Abount/index.tsx
+++ src/components/Abount/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import { S_Abount, Imagem } from './styled'
-
+import start from '../../../public/images/stars.jpg'  // esta imagem deveria se background
+import PlanetaMarte from '../../../public/images/mars.svg' // esta deveria ficar por cima
 
 export default function Abount() {
   return (
@@ -10,7 +12,8 @@ export default function Abount() {
 
       </Head>
       <S_Abount>
-          
+        <Imagem src={ start } />
+        <Image src={ PlanetaMarte } />
 
       </S_Abount>
 
--- src/components/Abount/styled.ts
+++ src/components/Abount/styled.ts
@@ -1,4 +1,4 @@
-
+import Image from 'next/image'
 import styled from "styled-components";
 
 
@@ -6,15 +6,12 @@ import styled from "styled-components";
 export const S_Abount = styled.div`
   height: 124.1rem;
   width: 100vw;
-  
-`
-
-export const Imagem = styled.div`
-
-
 
 `
 
-export const PlanetMart = styled.div`
+export const Imagem = styled(Image)`
+  position: absolute;
+  z-index: -1;
 
 `
+
